25a91ae3109c77175328db10d76d7513c5f8fbb1
Jon Lønne <jon@funkbit.no>
2013-02-20 13:39:16 +0100

    Minor: fixed indentation bork, removed unused import.

--- doctor/urls.py
+++ doctor/urls.py
@@ -1,14 +1,14 @@
 try:
-    from django.conf.urls import patterns, include, url
+    from django.conf.urls import patterns, url
 except ImportError:  # Django < 1.4 compatibility
     from django.conf.urls.defaults import *
 
 
 urlpatterns = patterns('doctor.views',
 
-	url(r'^health-check/$', 'health_check', name='doctor-health-check'),
-	url(r'^technical/$', 'technical_info', name='doctor-technical'),
-	url(r'^server-error/$', 'force_server_error', name='doctor-server-error'),
+    url(r'^health-check/$', 'health_check', name='doctor-health-check'),
+    url(r'^technical/$', 'technical_info', name='doctor-technical'),
+    url(r'^server-error/$', 'force_server_error', name='doctor-server-error'),
 
     url(r'^$', 'index', name='doctor-index'),
 
