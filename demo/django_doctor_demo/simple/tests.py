507790e739d10aff82d08da326281230f0855513
Benoît Bryon <benoit@marmelune.net>
2013-02-21 22:39:25 +0100

    Less code in demo's tests.

--- demo/django_doctor_demo/simple/tests.py
+++ demo/django_doctor_demo/simple/tests.py
@@ -6,48 +6,35 @@ from django.test import TestCase
 
 class HealthCheckViewTestCase(TestCase):
     """Test health check view."""
-    def test_fake_health_check(self):
-        """Fake healthcheck runs health checks (1 success, 1 error, 1 failure).
-
-        """
-        url = reverse('simple:health_check',
-                      args=['django_doctor_demo/simple/healthchecks/FakeHealthCheck'])
+    def assertHealthCheck(self, path, status_code=200, runs=1,
+                          errors=0, failures=0):
+        """Shortcut to test a health check view."""
+        url = reverse('simple:health_check', args=[path])
         response = self.client.get(url)
-        status_code = 500
         self.assertEqual(response.status_code, status_code)
-        self.assertContains(response, "3 tests run", status_code=status_code)
-        self.assertContains(response, "1 errors", status_code=status_code)
-        self.assertContains(response, "1 failures", status_code=status_code)
+        self.assertContains(response, "%d tests run" % runs,
+                            status_code=status_code)
+        self.assertContains(response, "%d errors" % errors,
+                            status_code=status_code)
+        self.assertContains(response, "%d failures" % failures,
+                            status_code=status_code)
+
+    def test_fake_health_check(self):
+        """Fake healthcheck runs (1 success, 1 error, 1 failure)."""
+        path = 'django_doctor_demo/simple/healthchecks/FakeHealthCheck'
+        self.assertHealthCheck(path, 500, runs=3, errors=1, failures=1)
 
     def test_fake_success(self):
         """Fake success returns HTTP 200."""
-        url = reverse('simple:health_check',
-                      args=['django_doctor_demo/simple/healthchecks/FakeHealthCheck/test_success'])
-        response = self.client.get(url)
-        status_code = 200
-        self.assertEqual(response.status_code, status_code)
-        self.assertContains(response, "1 tests run", status_code=status_code)
-        self.assertContains(response, "0 errors", status_code=status_code)
-        self.assertContains(response, "0 failures", status_code=status_code)
+        path = 'django_doctor_demo/simple/healthchecks/FakeHealthCheck/test_success'
+        self.assertHealthCheck(path, 200, runs=1, errors=0, failures=0)
 
     def test_fake_error(self):
         """Fake error returns HTTP 500."""
-        url = reverse('simple:health_check',
-                      args=['django_doctor_demo/simple/healthchecks/FakeHealthCheck/test_error'])
-        response = self.client.get(url)
-        status_code = 500
-        self.assertEqual(response.status_code, status_code)
-        self.assertContains(response, "1 tests run", status_code=status_code)
-        self.assertContains(response, "1 errors", status_code=status_code)
-        self.assertContains(response, "0 failures", status_code=status_code)
+        path = 'django_doctor_demo/simple/healthchecks/FakeHealthCheck/test_error'
+        self.assertHealthCheck(path, 500, runs=1, errors=1, failures=0)
 
     def test_fake_failure(self):
         """Fake failure returns HTTP 500."""
-        url = reverse('simple:health_check',
-                      args=['django_doctor_demo/simple/healthchecks/FakeHealthCheck/test_failure'])
-        response = self.client.get(url)
-        status_code = 500
-        self.assertEqual(response.status_code, status_code)
-        self.assertContains(response, "1 tests run", status_code=status_code)
-        self.assertContains(response, "0 errors", status_code=status_code)
-        self.assertContains(response, "1 failures", status_code=status_code)
+        path = 'django_doctor_demo/simple/healthchecks/FakeHealthCheck/test_failure'
+        self.assertHealthCheck(path, 500, runs=1, errors=0, failures=1)
